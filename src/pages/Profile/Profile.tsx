3a82cedea9563f609c2b4cc259d20dc87356a512
Утбанов Александр <utbanovag@it2g.ru>
2020-05-03 18:28:32 +0500

    add gallery

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -3,13 +3,14 @@ import { useSelector } from "react-redux";
 
 import "./style.scss";
 import { Profile as ProfileModel } from "../../shared/models";
+import { Gallery } from "./components";
 
 export const Profile: React.FC = () => {
   const profile: ProfileModel = useSelector((state: any) => state.profile);
 
   return (
     <div className="profile">
-      {profile.name}
+      <Gallery />
     </div>
   );
 };
--- src/pages/Profile/components/Gallery/Gallery.tsx
+++ src/pages/Profile/components/Gallery/Gallery.tsx
@@ -1,3 +1,57 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
-export const Gallery = () => {}
+import "./style.scss";
+import { Profile } from "../../../../shared/models";
+
+interface GalleryPhotoProps {
+    alt: string,
+    photo: string
+}
+
+const GalleryPhoto: React.FC<GalleryPhotoProps> = (props) => {
+    return (
+        <div className="gallery-photo">
+            <img src={props.photo} alt={props.alt} />
+        </div>
+    )
+}
+
+export const Gallery = () => {
+    const { gallery }: Profile = useSelector((state: any) => state.profile);
+    
+    // TODO: Надо учитывать что фоток может и не быть
+    const galleryLength = gallery?.length;
+
+    const renderPhotos = () => {
+        if (!!gallery && !!gallery.length) {
+            return (
+                <>
+                    {
+                        gallery.map((photo: string, i: number) => 
+                            <GalleryPhoto 
+                                photo={photo}
+                                alt={i.toString()}
+                                key={i}
+                            />
+                        )
+                    }
+                </>
+            )
+        }
+
+        // TODO: нужно показывать что фоток нет
+        return null;
+    }
+
+    return (
+        <div className="gallery">
+            <div className="galley-header">
+                <h3>Фото ({galleryLength}):</h3>
+            </div>
+            <div className="gallery-body">
+                { renderPhotos() }
+            </div>
+        </div>
+    )
+}
