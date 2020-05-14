ecc0ab507d41eceb95b9aa4b0360dc77d74fb940
Alexander Lomonosov <skilldill@yandex.ru>
2020-05-15 00:38:48 +0500

    Updates

---
+++ precache-manifest.81b227ddd313d297fc73715e600e18c9.js
@@ -0,0 +1,418 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a978527ba2cf6462e0509ceab52e4f34",
+    "url": "/dating-app/index.html"
+  },
+  {
+    "revision": "161a0f46923676c5e8d5",
+    "url": "/dating-app/static/css/49.a72581b3.chunk.css"
+  },
+  {
+    "revision": "384c116192b90d18e1c4",
+    "url": "/dating-app/static/css/main.ad7e58dd.chunk.css"
+  },
+  {
+    "revision": "6ffdfedccd0e79665ad4",
+    "url": "/dating-app/static/js/0.74966479.chunk.js"
+  },
+  {
+    "revision": "e982c169774021426276",
+    "url": "/dating-app/static/js/1.3e664540.chunk.js"
+  },
+  {
+    "revision": "3f00bd486503dbf525d4",
+    "url": "/dating-app/static/js/2.8330b3f2.chunk.js"
+  },
+  {
+    "revision": "92bc11d6d0e89a5c0d35",
+    "url": "/dating-app/static/js/3.4f913543.chunk.js"
+  },
+  {
+    "revision": "628f7775c6e5751fc8c1",
+    "url": "/dating-app/static/js/4.7c9cda7a.chunk.js"
+  },
+  {
+    "revision": "161a0f46923676c5e8d5",
+    "url": "/dating-app/static/js/49.471d82a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dating-app/static/js/49.471d82a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b99abb726472250c96",
+    "url": "/dating-app/static/js/5.59997014.chunk.js"
+  },
+  {
+    "revision": "b03c4619f3f01a81ca5d",
+    "url": "/dating-app/static/js/50.281a7e04.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/dating-app/static/js/50.281a7e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd5ac4a70fd10b8f8a18",
+    "url": "/dating-app/static/js/51.50137f23.chunk.js"
+  },
+  {
+    "revision": "a72c503a74de5242e81d",
+    "url": "/dating-app/static/js/52.36f03d2e.chunk.js"
+  },
+  {
+    "revision": "6659d9ce7b9df442e860",
+    "url": "/dating-app/static/js/53.f2921ea0.chunk.js"
+  },
+  {
+    "revision": "f510eb4c488893c2a67e",
+    "url": "/dating-app/static/js/54.a9140c22.chunk.js"
+  },
+  {
+    "revision": "13674ac040736dea5471",
+    "url": "/dating-app/static/js/55.5d1e8c41.chunk.js"
+  },
+  {
+    "revision": "4eb6a51bfca2dbdba5cd",
+    "url": "/dating-app/static/js/56.446b2d05.chunk.js"
+  },
+  {
+    "revision": "c9937ee37151a5d505bf",
+    "url": "/dating-app/static/js/57.a68ddf13.chunk.js"
+  },
+  {
+    "revision": "7e432698e5bcb260bb3c",
+    "url": "/dating-app/static/js/58.e5ad19ec.chunk.js"
+  },
+  {
+    "revision": "49edace99d46a0d8b3cd",
+    "url": "/dating-app/static/js/59.edd8c4d6.chunk.js"
+  },
+  {
+    "revision": "a1253db41226063a551a",
+    "url": "/dating-app/static/js/60.93fa9d7d.chunk.js"
+  },
+  {
+    "revision": "03028abd4fee3e9778eb",
+    "url": "/dating-app/static/js/61.4845ed43.chunk.js"
+  },
+  {
+    "revision": "8647b8f44864f04ada39",
+    "url": "/dating-app/static/js/62.8d8edc4d.chunk.js"
+  },
+  {
+    "revision": "462976603984432f9d86",
+    "url": "/dating-app/static/js/63.877fcf2f.chunk.js"
+  },
+  {
+    "revision": "bda845b0e6c82f33f849",
+    "url": "/dating-app/static/js/64.e6fea9b0.chunk.js"
+  },
+  {
+    "revision": "0d3d781f123d0c23deca",
+    "url": "/dating-app/static/js/65.4275bbda.chunk.js"
+  },
+  {
+    "revision": "9ab26fcb4aadf5462a7e",
+    "url": "/dating-app/static/js/66.bec12fee.chunk.js"
+  },
+  {
+    "revision": "4a00aa10cca27028a2a4",
+    "url": "/dating-app/static/js/67.afff5ba3.chunk.js"
+  },
+  {
+    "revision": "6828b924fa9f180f19f6",
+    "url": "/dating-app/static/js/68.ed583a5c.chunk.js"
+  },
+  {
+    "revision": "879dbd97cfbf65055231",
+    "url": "/dating-app/static/js/69.b6febab9.chunk.js"
+  },
+  {
+    "revision": "c85d998ccd63879f1b92",
+    "url": "/dating-app/static/js/70.75bfd9a2.chunk.js"
+  },
+  {
+    "revision": "687415f724091953cb5f",
+    "url": "/dating-app/static/js/71.a29fbfd5.chunk.js"
+  },
+  {
+    "revision": "fdc45cac8f118144eb8e",
+    "url": "/dating-app/static/js/72.d502a02d.chunk.js"
+  },
+  {
+    "revision": "252cc681dd159786dc7e",
+    "url": "/dating-app/static/js/73.0f533ecb.chunk.js"
+  },
+  {
+    "revision": "efa50c6374941cabf0e6",
+    "url": "/dating-app/static/js/74.587878d6.chunk.js"
+  },
+  {
+    "revision": "01515c3532877d144825",
+    "url": "/dating-app/static/js/75.3498c3e2.chunk.js"
+  },
+  {
+    "revision": "822f58a9e5ab95df3aae",
+    "url": "/dating-app/static/js/76.335cc260.chunk.js"
+  },
+  {
+    "revision": "be6df308fabc065b8dbe",
+    "url": "/dating-app/static/js/77.7738d65d.chunk.js"
+  },
+  {
+    "revision": "a4273229d73116be3956",
+    "url": "/dating-app/static/js/78.86c799c4.chunk.js"
+  },
+  {
+    "revision": "2a5feb722965fe7993ae",
+    "url": "/dating-app/static/js/79.84c3059a.chunk.js"
+  },
+  {
+    "revision": "20e6e2fdb49afa6b9953",
+    "url": "/dating-app/static/js/80.54124849.chunk.js"
+  },
+  {
+    "revision": "5bcd7e2900224275bef3",
+    "url": "/dating-app/static/js/81.59454b70.chunk.js"
+  },
+  {
+    "revision": "eddcd39b782d2ae8cc0f",
+    "url": "/dating-app/static/js/82.1d0013d8.chunk.js"
+  },
+  {
+    "revision": "5adf9fc74a32f0a1e36f",
+    "url": "/dating-app/static/js/83.bf43ff72.chunk.js"
+  },
+  {
+    "revision": "53a86eea6ac458098ac5",
+    "url": "/dating-app/static/js/84.e167a95d.chunk.js"
+  },
+  {
+    "revision": "63dc2af5260f8dea4f23",
+    "url": "/dating-app/static/js/85.a6666d9a.chunk.js"
+  },
+  {
+    "revision": "9cae40ff36dba2007ae5",
+    "url": "/dating-app/static/js/86.56c2c6ab.chunk.js"
+  },
+  {
+    "revision": "4bbaf478eeaea5aa7fee",
+    "url": "/dating-app/static/js/87.d8fc71d1.chunk.js"
+  },
+  {
+    "revision": "a1bdac22f6896da24873",
+    "url": "/dating-app/static/js/88.fc1ba08b.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/dating-app/static/js/88.fc1ba08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e0c62b1900219b4f99",
+    "url": "/dating-app/static/js/89.97ff3e43.chunk.js"
+  },
+  {
+    "revision": "509c8f5894d47930574d",
+    "url": "/dating-app/static/js/90.1f33f0bc.chunk.js"
+  },
+  {
+    "revision": "514c922eaa28517b832a",
+    "url": "/dating-app/static/js/91.28f1ccb3.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/dating-app/static/js/91.28f1ccb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "384c116192b90d18e1c4",
+    "url": "/dating-app/static/js/main.a4759570.chunk.js"
+  },
+  {
+    "revision": "bc7981f380263447562b",
+    "url": "/dating-app/static/js/runtime-main.195d3a65.js"
+  },
+  {
+    "revision": "37146f8cc3bcbc8bde74",
+    "url": "/dating-app/static/js/stencil-ion-avatar_3-ios-entry-js.c2a5c672.chunk.js"
+  },
+  {
+    "revision": "8e38c7e4e5f1097357eb",
+    "url": "/dating-app/static/js/stencil-ion-avatar_3-md-entry-js.31fa746c.chunk.js"
+  },
+  {
+    "revision": "5afe1b735ee5ed449087",
+    "url": "/dating-app/static/js/stencil-ion-back-button-ios-entry-js.fdb8bec0.chunk.js"
+  },
+  {
+    "revision": "bd84a2cf7574cad10ec2",
+    "url": "/dating-app/static/js/stencil-ion-back-button-md-entry-js.32992c44.chunk.js"
+  },
+  {
+    "revision": "7069f95b3b0451a625c3",
+    "url": "/dating-app/static/js/stencil-ion-backdrop-ios-entry-js.cf110ed8.chunk.js"
+  },
+  {
+    "revision": "77ddf4507a70125c6660",
+    "url": "/dating-app/static/js/stencil-ion-backdrop-md-entry-js.d68f6acc.chunk.js"
+  },
+  {
+    "revision": "cab1d750a7363353e18e",
+    "url": "/dating-app/static/js/stencil-ion-card_5-ios-entry-js.2dc6b497.chunk.js"
+  },
+  {
+    "revision": "6101d83903c1d2ff6b42",
+    "url": "/dating-app/static/js/stencil-ion-card_5-md-entry-js.fd73d9c5.chunk.js"
+  },
+  {
+    "revision": "fead822a0474ea8f8c65",
+    "url": "/dating-app/static/js/stencil-ion-checkbox-ios-entry-js.61c58a9e.chunk.js"
+  },
+  {
+    "revision": "baa2bcb5d0e3a1b53b4b",
+    "url": "/dating-app/static/js/stencil-ion-checkbox-md-entry-js.d9416389.chunk.js"
+  },
+  {
+    "revision": "a89232220c52153a10d9",
+    "url": "/dating-app/static/js/stencil-ion-chip-ios-entry-js.106d78a0.chunk.js"
+  },
+  {
+    "revision": "76313ac7201dbfdf1258",
+    "url": "/dating-app/static/js/stencil-ion-chip-md-entry-js.c78b5f54.chunk.js"
+  },
+  {
+    "revision": "cc49752a175251ce9b01",
+    "url": "/dating-app/static/js/stencil-ion-col_3-entry-js.59661fe3.chunk.js"
+  },
+  {
+    "revision": "303bc36c355c142f3427",
+    "url": "/dating-app/static/js/stencil-ion-img-entry-js.fbd5b5bb.chunk.js"
+  },
+  {
+    "revision": "382def271e382deb72ae",
+    "url": "/dating-app/static/js/stencil-ion-infinite-scroll_2-ios-entry-js.ee7ead10.chunk.js"
+  },
+  {
+    "revision": "b901744e8a455688a952",
+    "url": "/dating-app/static/js/stencil-ion-infinite-scroll_2-md-entry-js.12fcc07b.chunk.js"
+  },
+  {
+    "revision": "a59796dd434ed90fc3db",
+    "url": "/dating-app/static/js/stencil-ion-input-ios-entry-js.47ee5182.chunk.js"
+  },
+  {
+    "revision": "2fb312d88ba7fe649ecf",
+    "url": "/dating-app/static/js/stencil-ion-input-md-entry-js.0d5642b3.chunk.js"
+  },
+  {
+    "revision": "5b34a7c46a0e70df1457",
+    "url": "/dating-app/static/js/stencil-ion-loading-ios-entry-js.5bcce7a3.chunk.js"
+  },
+  {
+    "revision": "5d4ff4a0e4289aa03eae",
+    "url": "/dating-app/static/js/stencil-ion-loading-md-entry-js.773e24cc.chunk.js"
+  },
+  {
+    "revision": "c799bffc6b409b210c13",
+    "url": "/dating-app/static/js/stencil-ion-popover-ios-entry-js.080fe504.chunk.js"
+  },
+  {
+    "revision": "c429763116fb79a931ee",
+    "url": "/dating-app/static/js/stencil-ion-popover-md-entry-js.c64933a9.chunk.js"
+  },
+  {
+    "revision": "158661a85b2cf0780c68",
+    "url": "/dating-app/static/js/stencil-ion-progress-bar-ios-entry-js.de44a700.chunk.js"
+  },
+  {
+    "revision": "82f36429b7e1bd116856",
+    "url": "/dating-app/static/js/stencil-ion-progress-bar-md-entry-js.265faa7a.chunk.js"
+  },
+  {
+    "revision": "c62b8f4b4c3f9dd0a6af",
+    "url": "/dating-app/static/js/stencil-ion-radio_2-ios-entry-js.3cfdefbe.chunk.js"
+  },
+  {
+    "revision": "5b76390659cf79b1bbba",
+    "url": "/dating-app/static/js/stencil-ion-radio_2-md-entry-js.ede78dad.chunk.js"
+  },
+  {
+    "revision": "80b4397d0472d920921e",
+    "url": "/dating-app/static/js/stencil-ion-reorder_2-ios-entry-js.fafe02a5.chunk.js"
+  },
+  {
+    "revision": "9fe787ffba6d9e6b6c99",
+    "url": "/dating-app/static/js/stencil-ion-reorder_2-md-entry-js.009cd17c.chunk.js"
+  },
+  {
+    "revision": "4e02361da952419b6698",
+    "url": "/dating-app/static/js/stencil-ion-ripple-effect-entry-js.57dc9536.chunk.js"
+  },
+  {
+    "revision": "b52039b3a8726e236d75",
+    "url": "/dating-app/static/js/stencil-ion-spinner-entry-js.2c19958e.chunk.js"
+  },
+  {
+    "revision": "18e799ab71810809a428",
+    "url": "/dating-app/static/js/stencil-ion-split-pane-ios-entry-js.e9935da1.chunk.js"
+  },
+  {
+    "revision": "1a49305cf5ff5fd027e8",
+    "url": "/dating-app/static/js/stencil-ion-split-pane-md-entry-js.650beef9.chunk.js"
+  },
+  {
+    "revision": "afb28c11de81376cc66b",
+    "url": "/dating-app/static/js/stencil-ion-tab-bar_2-ios-entry-js.2fab7538.chunk.js"
+  },
+  {
+    "revision": "376a16e293731582b256",
+    "url": "/dating-app/static/js/stencil-ion-tab-bar_2-md-entry-js.9bc4475a.chunk.js"
+  },
+  {
+    "revision": "fa796c98be95db2ac1b4",
+    "url": "/dating-app/static/js/stencil-ion-tab_2-entry-js.eda3021f.chunk.js"
+  },
+  {
+    "revision": "02dc3a818c700cf28c4a",
+    "url": "/dating-app/static/js/stencil-ion-text-entry-js.9c8c2c58.chunk.js"
+  },
+  {
+    "revision": "2374bae2f1b5c858210b",
+    "url": "/dating-app/static/js/stencil-ion-textarea-ios-entry-js.945244a8.chunk.js"
+  },
+  {
+    "revision": "bf32f2af6504f3da06eb",
+    "url": "/dating-app/static/js/stencil-ion-textarea-md-entry-js.336da48d.chunk.js"
+  },
+  {
+    "revision": "7c13552ab76e8c7cc0f1",
+    "url": "/dating-app/static/js/stencil-ion-toggle-ios-entry-js.49fc6f5a.chunk.js"
+  },
+  {
+    "revision": "8c61fb1809cab43ef9ea",
+    "url": "/dating-app/static/js/stencil-ion-toggle-md-entry-js.bb841513.chunk.js"
+  },
+  {
+    "revision": "9c21bf24dea404137ed0",
+    "url": "/dating-app/static/js/stencil-ion-virtual-scroll-entry-js.6ce7d7d4.chunk.js"
+  },
+  {
+    "revision": "f330f110499a5aad3ee217ca09a9e900",
+    "url": "/dating-app/static/media/burger-outline.f330f110.svg"
+  },
+  {
+    "revision": "fa88da80fe37c24ea43c6928df73144e",
+    "url": "/dating-app/static/media/close-circle-outline.fa88da80.svg"
+  },
+  {
+    "revision": "afd69e68bb46952bebf3cea5690d5f66",
+    "url": "/dating-app/static/media/heart-outline.afd69e68.svg"
+  },
+  {
+    "revision": "829642a7d83e5ef52de7a4a89cfaad0a",
+    "url": "/dating-app/static/media/search-outline.829642a7.svg"
+  },
+  {
+    "revision": "f814d93f645be7496a28f8258d0cd7a6",
+    "url": "/dating-app/static/media/sync-outline.f814d93f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dating-app/precache-manifest.9e660f3b5c76f797e08e019b2e86a7bd.js"
+  "/dating-app/precache-manifest.81b227ddd313d297fc73715e600e18c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dating-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
