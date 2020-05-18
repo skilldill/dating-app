5d42fa0dc4e95c579ac5a97656744f56f8080485
Alexander Lomonosov <skilldill@yandex.ru>
2020-05-18 23:54:09 +0500

    add message input

--- src/pages/Chat/components/MessageForm/MessageForm.tsx
+++ src/pages/Chat/components/MessageForm/MessageForm.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./style.scss";
 import { MessageFormProps } from "./MessageForm.model";
+import attachIcon from "assets/icons/attach-outline.svg";
+import arrowUp from "assets/icons/arrow-up-outline.svg";
 
 export const MessageForm: React.FC<MessageFormProps> = (props) => {
     const { partner } = props;
@@ -9,7 +11,18 @@ export const MessageForm: React.FC<MessageFormProps> = (props) => {
     return (
         <div className="message-form">
             <form>
-                <input type="text" placeholder="Сообщение..."/>
+                <div className="input-file">
+                    <label htmlFor="chat-file">
+                        <img src={attachIcon} alt="attach"/>
+                    </label>
+                    <input type="file" id="chat-file" />
+                </div>
+                <div className="input-message">
+                    <input type="text" placeholder="Сообщение..." />
+                </div>
+                <button className="submit">
+                    <img src={arrowUp} alt="arrow"/>
+                </button>
             </form>
         </div>
     )
