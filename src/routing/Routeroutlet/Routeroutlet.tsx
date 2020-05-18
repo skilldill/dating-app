d1ca88dcf918bba67768723db96d11f7ecdf8540
Alexander Lomonosov <skilldill@yandex.ru>
2020-05-19 01:58:04 +0500

    return initial

--- src/routing/Routeroutlet/Routeroutlet.tsx
+++ src/routing/Routeroutlet/Routeroutlet.tsx
@@ -65,7 +65,7 @@ export const Routeroutlet = () => {
         onClick={handleClick}
       >
         <div className="routes">
-          <Route path="/" render={() => <Redirect to="/chat/3" />} />
+          <Route path="/" render={() => <Redirect to="/partners" />} />
           {routes.map((route:RouteProps, i: number) => 
             <Route key={i} {...route} />
           )}
