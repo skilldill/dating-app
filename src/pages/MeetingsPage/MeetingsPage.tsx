4c62229c49c7dc4c411caab99a8a69c0eec1380e
Утбанов Александр <utbanovag@it2g.ru>
2020-06-12 13:02:38 +0500

    add toggle meetings page

--- src/pages/MeetingsPage/MeetingsPage.tsx
+++ src/pages/MeetingsPage/MeetingsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 import { useSelector } from "react-redux";
 
 import "./style.scss";
@@ -6,8 +6,10 @@ import { Page } from "shared/components";
 import { MeetingsList, MeetingsFilter, MeetingsCalendar } from "./components";
 import { MEETING_STATUSES } from "shared/constants";
 import { Meeting } from "shared/models";
+import { ToggleRadio} from "shared/components";
 
 export const MeetingsPage = () => {
+    const [showCalendar, setShowCalendar] = useState(true);
     const [showStatus, setShowStatus] = useState<MEETING_STATUSES | null>(null);
     const [searchQuery, setSearchQuery] = useState('');
     const meetings = useSelector<any, Meeting[]>((state: any) => state.meetings.meetings);
@@ -18,31 +20,51 @@ export const MeetingsPage = () => {
         return meetings.filter((meeting) => meeting.description?.includes(searchQuery));
     } 
 
+    const toggleOptions = useMemo(() => [
+        { name: 'Календарь', value: 1 },
+        { name: 'Список', value: 2 },
+    ], [])
+
+    const handleToggle = () => {
+        setShowCalendar(!showCalendar);
+    }
+
     return (
         <Page>
-            <MeetingsCalendar meetings={meetings} />
-            {/* <div className="meetings-page">
-                <MeetingsFilter 
-                    onChangeStatuses={setShowStatus}
-                    onSearch={setSearchQuery}
+            <div className="meetings-header">
+                <ToggleRadio 
+                    values={toggleOptions}
+                    onChange={handleToggle}
                 />
-                {
-                    (showStatus !== MEETING_STATUSES.CONFIRMED) && (
-                        <MeetingsList 
-                            title="В ожидании"
-                            meetings={filtredMeetings(awaitingMeetings)}
+            </div>
+            {
+                showCalendar ?
+                <MeetingsCalendar meetings={meetings} /> :
+                (
+                    <div className="meetings-page">
+                        <MeetingsFilter 
+                            onChangeStatuses={setShowStatus}
+                            onSearch={setSearchQuery}
                         />
-                    )
-                }
-                {
-                    (showStatus !== MEETING_STATUSES.AWAITING) && (
-                        <MeetingsList 
-                            title="Подтверждённые"
-                            meetings={filtredMeetings(confirmedMeetings)}
-                        />
-                    )
-                }
-            </div> */}
+                        {
+                            (showStatus !== MEETING_STATUSES.CONFIRMED) && (
+                                <MeetingsList 
+                                    title="В ожидании"
+                                    meetings={filtredMeetings(awaitingMeetings)}
+                                />
+                            )
+                        }
+                        {
+                            (showStatus !== MEETING_STATUSES.AWAITING) && (
+                                <MeetingsList 
+                                    title="Подтверждённые"
+                                    meetings={filtredMeetings(confirmedMeetings)}
+                                />
+                            )
+                        }
+                    </div>
+                )
+            }
         </Page>
     )
 }
